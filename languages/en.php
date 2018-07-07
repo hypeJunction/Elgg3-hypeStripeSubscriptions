dc8dc56579f72943e7c87ffcfccdbaf1c0f7a795
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-07-07 14:23:25 +0200

    fix(lang): add missing translation

--- languages/en.php
+++ languages/en.php
@@ -4,4 +4,5 @@ return [
 	'subscriptions:stripe:import' => 'Sync Stripe Plans',
 	'subscriptions:stripe:import:success' => '%s subscription plans have been imported; %s plan exported',
 	'subscriptions:stripe:refunded' => '%s %s were refunded to your card',
+	'subscriptions:stripe:error:payment_required' => 'Payment was not successful',
 ];
